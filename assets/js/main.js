40fe243f85ec12bb912fe7a7ec9dec50b687f24b
mahnoorjaved2 <mahnoorjaved240@gmail.com>
2026-06-25 14:45:42 +0500

    second commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,121 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
 
+      /*===== MENU SHOW =====*/
+      if(navToggle){
+          navToggle.addEventListener('click', () =>{
+              navMenu.classList.add('show-menu')
+          })
+      }
+
+      /*===== MENU HIDDEN =====*/
+      if(navClose){
+          navClose.addEventListener('click', () =>{
+                navMenu.classList.remove('show-menu')
+            })
+        }
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
 
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== HOME TYPED JS ===============*/
-
+const typedHome = new Typed('#home-typed', {
+    strings: ['Web Developer', 'Front-End Developer', 'Graphic Designer', 'Video Editor'],
+    typeSpeed: 80,
+    backSpeed: 40,
+    backDelay: 2000,
+    loop: true,
+    cursorChar: '_',
+})
 
 /*=============== ADD SHADOW HEADER ===============*/
-
+const shadowHeader = () => {
+    const header = document.getElementById('header')
+    //Add a class if the bottom offset is greator than 50 of the viewport
+    this.scrollY >= 50 ? header.classList.add('shadow-header')
+                       : header.classList.remove('shadow-header')
+}
+window.addEventListener('scroll', shadowHeader)
 
 /*=============== CONTACT EMAIL JS ===============*/ 
+const contactForm = document.getElementById('contact-form'),
+    contactMessage = document.getElementById('contact-message')
 
+    emailjs.init('O6LGPHaYK-by5TtDE')
+
+    const sendEmail = (e) => {
+    e.preventDefault()
+    emailjs.sendForm('service_nknzj5a','template_g0li3wa','#contact-form')
+    .then(() => {
+        contactMessage.textContent = 'Message Sent Successfully!'
+        setTimeout(() => {
+        contactMessage.textContent = ''
+        }, 5000)
+
+        contactForm.reset()
+    }, () => {
+
+        contactMessage.textContent = 'Message not sent (service error)!'
+        setTimeout(() => {
+        contactMessage.textContent = ''
+        }, 5000)
+    })
+
+}
+if(contactForm){
+contactForm.addEventListener('submit', sendEmail) 
+}
 
 /*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
 
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
 
+const scrollActive = () =>{
+    const scrollDown = window.scrollY
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionClass = document.querySelector('.nav__menu a[href=' + sectionId + ']')
+
+        if(scrollDown > sectionTop && scrollDown <=  sectionTop + sectionHeight){
+            sectionClass.classList.add('active-link')
+        }else{
+            sectionClass.classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+})
+
+sr.reveal('.home__content, .resume__content:nth-child(1), .footer__container')
+sr.reveal('.home__data, .resume__content:nth-child(2)', {delay: 300, origin: 'bottom'})
+
+sr.reveal('.about__content, .contact__content', {origin: 'bottom'})
+sr.reveal('.about__image, .contact__form', {delay: 300})
+
+sr.reveal('.projects__card', {interval: 100})
